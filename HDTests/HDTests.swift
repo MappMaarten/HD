668375b9430826013cb90c1e7084d9cbaa2c3b9e
Maarten <maarten_m@icloud.com>
2026-01-18 13:17:12 +0100

    Initial Commit

---
+++ HDTests/HDTests.swift
@@ -0,0 +1,17 @@
+//
+//  HDTests.swift
+//  HDTests
+//
+//  Created by Maarten van Middendorp on 18/01/2026.
+//
+
+import Testing
+@testable import HD
+
+struct HDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
